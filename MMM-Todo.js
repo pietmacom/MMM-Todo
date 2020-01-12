1306af1e7c8871139106f1e41e27acb6638f44f4
Martina Pietruschka <post@martinapietruschka.de>
2020-01-12 15:42:15 +0100

    nodate

--- MMM-Todo.js
+++ MMM-Todo.js
@@ -276,9 +276,100 @@ Module.register("MMM-Todo", {
 
 				eventWrapper.appendChild(titleWrapper);
 			} else {
+				var timeWrapper = document.createElement("td");
 
 				eventWrapper.appendChild(titleWrapper);
-				
+				//console.log(event.today);
+				var now = new Date();
+				// Define second, minute, hour, and day variables
+				var oneSecond = 1000; // 1,000 milliseconds
+				var oneMinute = oneSecond * 60;
+				var oneHour = oneMinute * 60;
+				var oneDay = oneHour * 24;
+				if (event.fullDayEvent) {
+					//subtract one second so that fullDayEvents end at 23:59:59, and not at 0:00:00 one the next day
+					event.endDate -= oneSecond;
+					if (event.today) {
+						timeWrapper.innerHTML = this.capFirst(this.translate("TODAY"));
+					} else if (event.startDate - now < oneDay && event.startDate - now > 0) {
+						timeWrapper.innerHTML = this.capFirst(this.translate("TOMORROW"));
+					} else if (event.startDate - now < 2 * oneDay && event.startDate - now > 0) {
+						if (this.translate("DAYAFTERTOMORROW") !== "DAYAFTERTOMORROW") {
+							timeWrapper.innerHTML = this.capFirst(this.translate("DAYAFTERTOMORROW"));
+						} else {
+							timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").fromNow());
+						}
+					} else {
+						/* Check to see if the user displays absolute or relative dates with their events
+						* Also check to see if an event is happening within an 'urgency' time frameElement
+						* For example, if the user set an .urgency of 7 days, those events that fall within that
+						* time frame will be displayed with 'in xxx' time format or moment.fromNow()
+						*
+						* Note: this needs to be put in its own function, as the whole thing repeats again verbatim
+						*/
+						if (this.config.timeFormat === "absolute") {
+							if ((this.config.urgency > 1) && (event.startDate - now < (this.config.urgency * oneDay))) {
+								// This event falls within the config.urgency period that the user has set
+								timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").from(moment().format("YYYYMMDD")));
+							} else {
+								timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").format(this.config.fullDayEventDateFormat));
+							}
+						} else {
+							timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").from(moment().format("YYYYMMDD")));
+						}
+					}
+					if(this.config.showEnd){
+						timeWrapper.innerHTML += "-" ;
+						timeWrapper.innerHTML += this.capFirst(moment(event.endDate  , "x").format(this.config.fullDayEventDateFormat));
+					}
+				} else {
+					if (event.startDate >= new Date()) {
+						if (event.startDate - now < 2 * oneDay) {
+							// This event is within the next 48 hours (2 days)
+							if (event.startDate - now < this.config.getRelative * oneHour) {
+								// If event is within 6 hour, display 'in xxx' time format or moment.fromNow()
+								timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").fromNow());
+							} else {
+								if(this.config.timeFormat === "absolute" && !this.config.nextDaysRelative) {
+									timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").format(this.config.dateFormat));
+								} else {
+									// Otherwise just say 'Today/Tomorrow at such-n-such time'
+									timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").calendar());
+								}
+							}
+						} else {
+							/* Check to see if the user displays absolute or relative dates with their events
+							* Also check to see if an event is happening within an 'urgency' time frameElement
+							* For example, if the user set an .urgency of 7 days, those events that fall within that
+							* time frame will be displayed with 'in xxx' time format or moment.fromNow()
+							*
+							* Note: this needs to be put in its own function, as the whole thing repeats again verbatim
+							*/
+							if (this.config.timeFormat === "absolute") {
+								if ((this.config.urgency > 1) && (event.startDate - now < (this.config.urgency * oneDay))) {
+									// This event falls within the config.urgency period that the user has set
+									timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").fromNow());
+								} else {
+									timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").format(this.config.dateFormat));
+								}
+							} else {
+								timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").fromNow());
+							}
+						}
+					} else {
+						timeWrapper.innerHTML = this.capFirst(
+							this.translate("RUNNING", {
+								fallback: this.translate("RUNNING") + " {timeUntilEnd}",
+								timeUntilEnd: moment(event.endDate, "x").fromNow(true)
+							})
+						);
+					}
+					if (this.config.showEnd) {
+						timeWrapper.innerHTML += "-";
+						timeWrapper.innerHTML += this.capFirst(moment(event.endDate, "x").format(this.config.dateEndFormat));
+
+					}
+				}
 				//timeWrapper.innerHTML += ' - '+ moment(event.startDate,'x').format('lll');
 				//console.log(event);
 				var timeClass = this.timeClassForUrl(event.url);
